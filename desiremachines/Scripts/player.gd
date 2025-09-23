bcc23fc24745d22f22c438d475c57745d3be8c6c
beaflowers <157410173+beaflowers@users.noreply.github.com>
2025-09-23 12:59:00 -0400

    Big Movement Update
    Made some tweaks I was thinking about to player/players movement.
    
    Before, a disagreement would cause a total halt in movement - no one would go anywhere. It also just felt very stiff and predictable. Now, there's a random variable (generated between -1 and 10, unsure if those will be final numbers? 10 seems a bit high) that gets multiplied by the left or right movement vector. This means that the right or left player suddenly has much more power behind their thrust (ahem) when picking a direction. It also adds a bit of chaos in terms of speed - makes it funny to be suddenly zooming off unexpectedly fast. It /feels/ fun!
    
    Learned how to set up a timer in Godot that controls when these random multipliers shift (currently every 7 seconds). Pretty easy, it's a node and then call a function in the script to update random variables when timer runs out. I love Godot.
    
    I like this movement mechanic in conjunction with consent and ideation of sexuality... Some times you really are pulled along by another person's desires. Often this is a way you get to discover something new that you like, and it's not always strictly consensual (in the binary yes/no sense, which... is not a particularly helpful way to think about consent, imo).

--- desiremachines/Scripts/player.gd
+++ desiremachines/Scripts/player.gd
@@ -4,18 +4,30 @@ extends CharacterBody2D
 # Exported variable allows you to change the speed directly in the Inspector.
 @export var speed = 200.0
 
+var left_random_multiplier = 1.0
+var right_random_multiplier = 1.0
+
 func _ready():
 	print("Script running!")
+	_update_random()
+	
+func _update_random():
+	#introduce randomness into player movement
+	left_random_multiplier = randf_range(-2, 10)
+	right_random_multiplier = randf_range(-2, 10) 
+	print("Random Multipliers - Left: ", left_random_multiplier, " Right: ", right_random_multiplier)
 	
 func get_input():
 	#left player movement
 	var leftPlayer = Input.get_vector("leftplayer_left", "leftplayer_right", "leftplayer_up", "leftplayer_down")
-	
 	#right player movement
 	var rightPlayer = Input.get_vector("rightplayer_left", "rightplayer_right", "rightplayer_up", "rightplayer_down")
 	
-	#average for direction
+	#multiples by random number generator to create chaos
+	leftPlayer = leftPlayer * left_random_multiplier
+	rightPlayer = rightPlayer * right_random_multiplier
 	
+	#average for direction
 	var direction = (leftPlayer + rightPlayer) / 2
 	
 	velocity = direction * speed
@@ -24,3 +36,7 @@ func get_input():
 func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	#timer length is set in Node rather than in code
+	_update_random() 
