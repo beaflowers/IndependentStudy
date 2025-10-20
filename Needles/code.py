135eb27b47a3b0a1354cdc6cb065dee2a375f819
beaflowers <157410173+beaflowers@users.noreply.github.com>
2025-10-20 11:39:44 -0400

    Tilt sensor + disaster
    Got code and circuit tested and written for tilt circuit. Idea is that when wire connected to needle is moved to a point where it "tilts" (I guess in the sensors case, goes from flat to 90 degrees?) it would pull on the needle in someone's skin and then light up an LED. I was honestly more excited about this than the touch capacitive sensor because I really don't think that one is gonna work.
    
    Unfortunately as I was trying to wire it to the needle I broke the pin off the sensor. Very demoralized and annoyed! Unsure where to get another sensor that's convenient and quick. Had someone coming over today to help me test it.
    
    Honestly - this happened because I was rushing. I had only a few hours to work and I was panicking and being too rough. I should have spent some time looking for electrical tape or a better means of attaching wires together than pliers.
    
    Follow up plans: see if Elio has a tilt sensor I can borrow? Perhaps use a different module that works with an accelerometer that will allow for more sensitivity in tilt function (probably better ultimately, to make sure not hurting someone too much?) See what other sensors I have? See if my test subject can reschedule?

--- Needles/code.py
+++ Needles/code.py
@@ -12,12 +12,22 @@ print("Still going boss")
 i2c = busio.I2C(scl=board.GP7, sda=board.GP6)
 touch_pad = adafruit_mpr121.MPR121(i2c)
 
+#initialize GP14 as tilt, circuit closed when flat 
+tilt_switch = digitalio.DigitalInOut(board.GP14)
+tilt_switch.direction = digitalio.Direction.INPUT
+
 #track for capacative sensor, starts as false and is set within function
 last_touched = [False] * 12
 
+#track for tilt sensor
+last_state_tilt = False
+
 #setup LED
-led = digitalio.DigitalInOut(board.GP16)
-led.direction = digitalio.Direction.OUTPUT
+touch_led = digitalio.DigitalInOut(board.GP16)
+touch_led.direction = digitalio.Direction.OUTPUT
+
+tilt_led = digitalio.DigitalInOut(board.GP17)
+tilt_led.direction = digitalio.Direction.OUTPUT
 
 def touched():
     for i in range(12):
@@ -32,9 +42,28 @@ def touched():
         last_touched[i] = currently_touched
 
 def ledOn():
-    led.value = touch_pad[11].value 
+    touch_led.value = touch_pad[11].value
+    tilt_led.value = not tilt_switch.value
+    
+
+def tilt():
+    global last_state_tilt
+    current_state = not tilt_switch.value
+    
+    #only trigger on state change
+    if current_state != last_state_tilt:
+        time.sleep(0.05) #debounce
+        if current_state == (not tilt_switch.value):
+            last_state_tilt = current_state
+            if current_state:
+                print("Tilted!")
+                tilt_led.value = True
+            else:
+                print("Flat")
+                tilt_led.value = False
 
 while True:
     touched()
+    tilt()
     ledOn()
     time.sleep(0.5)
