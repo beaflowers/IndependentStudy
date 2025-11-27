0c78f7efc180ecc9c44a361f72d80aceea181046
beaflowers <157410173+beaflowers@users.noreply.github.com>
2025-11-27 11:58:53 -0500

    Adding documentation and I guess I added something to a comment in the code?

--- Needles/code.py
+++ Needles/code.py
@@ -1,18 +1,7 @@
 import board
 import digitalio
 import analogio
-import touchio
-import busio
 import time
-import adafruit_mpr121
-from adafruit_mpu6050 import MPU6050
-import math
-
-#setting up i2c bus - possibly no longer needed
-#setting up i2c sensors (MPR121, MPU6050)
-#i2c = busio.I2C(scl=board.GP7, sda=board.GP6)
-#touch_pad = adafruit_mpr121.MPR121(i2c)
-#tilt_sensor = MPU6050(i2c)
 
 #initialize GP14 as tilt, circuit closed when flat 
 tilt_switch = digitalio.DigitalInOut(board.GP14)
@@ -21,7 +10,7 @@ tilt_switch.direction = digitalio.Direction.INPUT
 #track for tilt sensor
 last_state_tilt = False
 
-#configure analog input pins
+#configure analog input pins (photoresistor)
 light = analogio.AnalogIn(board.GP27)
 
 #setup for photoresistor
@@ -44,7 +33,6 @@ light_led.direction = digitalio.Direction.OUTPUT
 touch_led = digitalio.DigitalInOut(board.GP20)
 touch_led.direction = digitalio.Direction.OUTPUT
 
-
     
 def tilt():
     global last_state_tilt
@@ -65,23 +53,6 @@ def tilt():
                 print("Flat")
                 tilt_led.value = False
 
-                
-def tilt_sense():
-    xAccel = round(tilt_sensor.acceleration[0], 1)
-    yAccel = round(tilt_sensor.acceleration[1], 1)
-    zAccel = round(tilt_sensor.acceleration[2], 1)
-    
-    #the math to get the degrees of tilt
-    mag = math.sqrt(xAccel**2 + yAccel**2 + zAccel**2)
-    zNormalized = zAccel / mag
-    
-    theta = math.acos(zNormalized) #only takes values between -1 and 1, so had to do that other stuff to compensate for gravity
-    thetaDeg = math.degrees(theta)
-    
-    
-    #print(f"x: {xAccel} m/s^2, y: {yAccel} m/s^2, z: {zAccel} m/s^2")
-    print("Tilt angle: ", thetaDeg)
-
 def photo_sense():
     global light_threshold, light_baseline
     light_test = light_baseline + light_threshold #changes if it gets darker
@@ -101,9 +72,7 @@ def read_touch():
     
 while True:
     tilt()
-    #tilt_sense()
-    #photo_sense()
-    #motion()
+    photo_sense()
     read_touch()
     
     time.sleep(0.5)
